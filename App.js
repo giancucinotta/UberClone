653925dc54aff2ad588ad0cd1fc9bfe6755c5379
Giancucinotta <giancucinotta@gmail.com>
2021-09-10 23:54:08 -0300

    -Redux configuration

--- App.js
+++ App.js
@@ -1,20 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { store } from './store';
+
+// 1) Set up redux toolkit
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App! Ahora! JEJEJEJ</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text >Let's build UBER</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgba(231,243,217,255)',
     alignItems: 'center',
     justifyContent: 'center',
   },
